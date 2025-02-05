058a0d891f0820c85e249ffe7b5fb75fff763f2f
Shashank Dave <shashankdave@Shashanks-Laptop.local>
2025-02-05 11:02:26 +0530

    Changes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const hbs = require("hbs");
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 const viewTemplates = path.join(__dirname, "../templates/views");
 const publicDirectoryPath = path.join(__dirname, "../public");
 const partialsPath = path.join(__dirname, "../templates/partials");
@@ -51,6 +53,6 @@ app.get("*", (req, res) => {
   res.send("404 ERROR: Page not found");
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
